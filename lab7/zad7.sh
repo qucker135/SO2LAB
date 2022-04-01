d0ea5ddc3c6ffd98fbe64fbe2b6e170be7597dea
qucker <Bartosz Matysiak>
2022-04-01 23:14:35 +0200

    Lab7 - 8/10 done

---
+++ lab7/zad7.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+# Systemy operacyjne 2 – laboratorium nr 7 – semestr letni 2020/2021
+#
+# Celem zajęć jest zapoznanie się z wyrażeniami regularnymi, wykorzystując
+# przy tym narzędzia awk i grep oraz wszystkie inne, poznane na zajęciach.
+#
+# Tradycyjnie nie przywiązujemy zbyt dużej wagi do środowiska roboczego.
+# Zakładamy, że format i układ danych w podanych plikach nie ulega zmianie,
+# ale same wartości, inne niż podane wprost w treści zadań, mogą ulec zmianie,
+# a przygotowane zadania wciąż powinny działać poprawnie (robić to, co trzeba).
+#
+# Wszystkie chwyty dozwolone, ale w wyniku ma powstać tylko to, o czym jest
+# mowa w treści zadania – nie generujemy żadnych dodatkowych plików pośrednich.
+#
+
+#
+# Zadanie 7.
+# Przeanalizować zawartość pliku `dodatkowe/kant.txt` i wyodrębnić z niego
+# możliwe dane kontaktowe – obecne tam adresy e-mail oraz numery telefonów.
+# Numery telefonów mogą być rozdzielone pomocniczymi separatorami, dodanymi
+# dla czytelności, a także być poprzedzone kodami kraju – należy je wtedy
+# także wyodrębnić.
+#
+
+grep -o -e "\(\([0-9]\{2\}\|+\)[0-9]\{2\}[ -]\?\)\?[0-9]\{3\}[ -]\?[0-9]\{3\}[ -]\?[0-9]\{3\}" dodatkowe/kant.txt
+grep -o -e "\([a-z0-9-]\+\.\)*[a-z0-9-]\+@[a-z0-9-]\+\(\.[a-z0-9-]\+\)*" dodatkowe/kant.txt
